420c906ca3b635a1a33e409d927d633ca7a36045
Aman Ahmed <aman.ahmed97@gmail.com>
2023-06-21 12:22:01 -0400

    blind 75 list solutions

--- leetcode_solutions/linked_list/Linked List Cycle.py
+++ leetcode_solutions/linked_list/Linked List Cycle.py
@@ -59,3 +59,14 @@ class Solution:
             trav = trav.next
 
         return False
+
+class Solution2:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
---
+++ leetcode_solutions/linked_list/Remove Nth Node From End of List.py
@@ -0,0 +1,78 @@
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+    The number of nodes in the list is sz.
+    1 <= sz <= 30
+    0 <= Node.val <= 100
+    1 <= n <= sz
+
+Follow up: Could you do this in one pass?
+"""
+
+from typing import *
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        l = 0
+        ctr = 0
+        curr = head
+        prev = curr
+
+        while curr:
+            l += 1
+            curr = curr.next
+        print('l', l)
+        curr = head
+        while ctr < (l - n):
+            prev = curr
+            curr = curr.next
+            ctr += 1
+
+        prev.next = curr.next
+
+        if (l - n) == 0:
+            return head.next
+
+        return head
+
+class Solution2:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        # delete
+        left.next = left.next.next
+        return dummy.next
