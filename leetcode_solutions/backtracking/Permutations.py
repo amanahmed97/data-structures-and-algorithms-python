b7353a245b14121c3ac8bb78147aac8cfb516630
Aman Ahmed <aman.ahmed97@gmail.com>
2023-09-09 18:15:04 -0400

    leetcode blind 75 solutions

---
+++ leetcode_solutions/backtracking/Permutations.py
@@ -0,0 +1,68 @@
+"""
+https://leetcode.com/problems/permutations/
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+
+
+Constraints:
+
+    1 <= nums.length <= 6
+    -10 <= nums[i] <= 10
+    All the integers of nums are unique.
+"""
+
+from typing import *
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        visited = set()
+        res = []
+        subset = []
+
+        self.backtrack(nums, visited, subset, res)
+        return res
+
+    def backtrack(self, nums, visited, subset, res):
+        if len(subset) == len(nums):
+            res.append(subset)
+        for i in range(len(nums)):
+            if i not in visited:
+                visited.add(i)
+                self.backtrack(nums, visited, subset + [nums[i]], res)
+                visited.remove(i)
+
+    def permuteNC(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+
+            res.extend(perms)
+            nums.append(n)
+
+        return res
---
+++ leetcode_solutions/backtracking/Subsets II.py
@@ -0,0 +1,55 @@
+"""
+https://leetcode.com/problems/subsets-ii/description/
+
+Given an integer array nums that may contain duplicates, return all possible
+subsets
+(the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+
+
+Constraints:
+
+    1 <= nums.length <= 10
+    -10 <= nums[i] <= 10
+"""
+
+from typing import *
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+                i += 1
+
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+
+
