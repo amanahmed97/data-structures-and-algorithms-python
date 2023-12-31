f7ea53feaa61055fac74e7f678511012b265edee
Aman Ahmed <aman.ahmed97@gmail.com>
2023-12-30 20:16:08 -0500

    Blind 75 / NC 150 solutions

--- leetcode_solutions/graphs_dfs/Flatten Binary Tree to Linked List.py
+++ leetcode_solutions/graphs_dfs/Flatten Binary Tree to Linked List.py
@@ -2,7 +2,8 @@
 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/?envType=study-plan-v2&envId=top-interview-150
 Given the root of a binary tree, flatten the tree into a "linked list":
 
-    The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+    The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the
+    list and the left child pointer is always null.
     The "linked list" should be in the same order as a pre-order traversal of the binary tree.
 
 
--- leetcode_solutions/linked_list/Swap Nodes in Pairs.py
+++ leetcode_solutions/linked_list/Swap Nodes in Pairs.py
@@ -1,7 +1,8 @@
 """
 https://leetcode.com/problems/swap-nodes-in-pairs/description/
 
-Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying
+the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
 
 
@@ -31,10 +32,10 @@ Constraints:
 from typing import *
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0,head)
