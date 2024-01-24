991b9864936a9327f4001c144388965a12a751f1
Aman Ahmed <aman.ahmed97@gmail.com>
2024-01-24 07:08:02 -0500

    Blind 75 / NC 150 solutions

--- leetcode_solutions/linked_list/Delete the Middle Node of a Linked List.py
+++ leetcode_solutions/linked_list/Delete the Middle Node of a Linked List.py
@@ -54,6 +54,27 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+class Solution1:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        slow, fast = head, head
+        prev = head
+
+        if not head.next:
+            return None
+
+        while fast and fast.next:
+            fast = fast.next.next
+            prev = slow
+            slow = slow.next
+
+        prev.next = slow.next
+
+        return head
+
+
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         ctr = 0
--- leetcode_solutions/trees/Path Sum.py
+++ leetcode_solutions/trees/Path Sum.py
@@ -1,7 +1,8 @@
 """
 https://leetcode.com/problems/path-sum/description/
 
-Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that
+adding up all the values along the path equals targetSum.
 A leaf is a node with no children.
 
 Example 1:
