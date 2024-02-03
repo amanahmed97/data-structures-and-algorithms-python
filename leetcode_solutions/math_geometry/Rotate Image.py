b2e07fcad46e2d42cf4674551411dee2b9df06e5
Aman Ahmed <aman.ahmed97@gmail.com>
2024-02-03 10:09:47 -0500

    Blind 75 / NC 150 solutions

--- leetcode_solutions/math_geometry/Rotate Image.py
+++ leetcode_solutions/math_geometry/Rotate Image.py
@@ -1,9 +1,8 @@
 """
 https://leetcode.com/problems/rotate-image/
 You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
-
-You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
-
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate
+another 2D matrix and do the rotation.
 
 
 Example 1:
@@ -15,7 +14,6 @@ Example 2:
 
 Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
 Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
-
 
 
 Constraints:
@@ -52,3 +50,28 @@ class Solution:
 
             l += 1
             r -= 1
+
+
+class Solution2:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        r,c = 0, len(matrix)-1
+
+        while r<c:
+            # print(r,c)
+            # Layer wise
+            for i in range(c-r):
+                # save top
+                temp = matrix[r][r+i]
+                # move left to top
+                matrix[r][r+i] = matrix[c-i][r]
+                # move bottom to left
+                matrix[c-i][r] = matrix[c][c-i]
+                # move right to bottom
+                matrix[c][c-i] = matrix[r+i][c]
+                # move saved top to right
+                matrix[r+i][c] = temp
+            r+=1
+            c-=1
--- leetcode_solutions/math_geometry/Set Matrix Zeroes.py
+++ leetcode_solutions/math_geometry/Set Matrix Zeroes.py
@@ -2,7 +2,6 @@
 https://leetcode.com/problems/set-matrix-zeroes/description/
 
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
-
 You must do it in place.
 
 
